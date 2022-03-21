1dfe7b5f34d75935fd413476c39afc8d3f7c58c9
Madhusuthanan-B <madhub081011@gmail.com>
2022-03-22 05:01:23 +0530

    Using a court type

--- Tennis-Simulator-App/src/app/tennis/ground/ground.component.ts
+++ Tennis-Simulator-App/src/app/tennis/ground/ground.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { CourtType } from '../models/court-type';
 
 @Component({
   selector: 'app-ground',
@@ -8,7 +9,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class GroundComponent implements OnInit {
 
-  @Input() courtType: 'grass-court' | 'clay-court' | 'synthetic-court' = 'grass-court';
+  @Input() courtType: CourtType = 'grass';
 
   constructor() { }
 
---
+++ Tennis-Simulator-App/src/app/tennis/models/court-type.ts
@@ -0,0 +1,1 @@
+export type CourtType = 'grass' | 'clay' | 'synthetic';
